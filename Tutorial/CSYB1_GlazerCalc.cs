175effa518a40353d008f51473030cc162201bf9
Victorli888 <victorli7462@gmail.com>
2020-11-12 22:03:30 -0800

    define each line with function

--- Tutorial/CSYB1_GlazerCalc.cs
+++ Tutorial/CSYB1_GlazerCalc.cs
@@ -21,3 +21,26 @@ class CSYB1_GlazerCalc
 
     }
 }
+
+/* For Learning Purposes lets go through each line to understand each item and it's function
+
+using System;
+- tells the compilier to use things from System namespace.
+- namespace:  is where particular names have meaning
+- Console: class allows for things to be written on screen to be viewed in front of the user
+- static: makes sure that the following method is ALWAYS present.
+- Void: The method were about to describe does not return anything of interest. Meaning it'll do its job and finish
+- Main: There is always one and must always be one, This is where the system knows where to start.
+- (): Closed pair of brackets tells the Method Main doesn't have any parameters
+- {}: Lumps code blocks and tells the complier that } is the end of that method 
+- double: "Double Precision floating number"
+- varaible names; width, height woodLength etc
+- ; this marks the end of a statement
+- Console. : the method is a part of the object, Console which manages user input and output
+- . : the full stop dot this seperates the object identifier with the method identifier
+- Readline waits for the user to enter a line of text and press the Enter key
+- double. perform double precision floating point number on widthString by exposing Parse method
+- Parse method - converting the string to a double percision floating point number
+- (widthString); - serves as a parameter for Parse method
+- 
+*/
--- Tutorial/helloworld.cs
+++ Tutorial/helloworld.cs
@@ -4,7 +4,7 @@ namespace Tutorial
 {
     class hello_world
     {
-        static void Main()  // Static - method belongs to "class Program" Void - method will not have a return value
+        static void Main1()  // Static - method belongs to "class Program" Void - method will not have a return value
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is used for a test commit.");
@@ -13,5 +13,5 @@ namespace Tutorial
 }
 
 // The Console class is used to represent the standard input, output, and error streams for console applications.
-// Use namespace to provide a way to keep one set on names seperate from another. I.e class names declared in one namespace 
+// Use namespace to provide a way to keep one set on names separate from another. I.e class names declared in one namespace 
 // won't be conflict with class names declared in another.
